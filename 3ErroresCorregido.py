a0710df7f2711af2207bf0aa510389c52b9f34a5
dvillazu27 <61160481+dvillazu27@users.noreply.github.com>
2020-02-26 20:47:18 -0600

    Punto 8 Tarea 1
    Errores corregidos

---
+++ 3ErroresCorregido.py
@@ -0,0 +1,19 @@
+def Menu():
+
+    print("      *****MENU*****")
+    print("      *            *")
+    print("      *            *")
+    print("      * BIENVENIDO *")
+    print("      *            *")
+    print("      *            *")
+    print("      *            *")
+    print("      *            *")
+    print("      *   DIGITE   *")
+    print("      *     SU     *")
+
+    print("      *    ORDEN   *")
+    print("      *            *")
+    print("      *            *")
+    print("      *            *")
+    print("      *            *")
+    print("      ****GRACIAS***")
