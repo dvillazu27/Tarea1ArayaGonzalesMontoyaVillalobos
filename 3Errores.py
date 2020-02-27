a0a99f2a6fdf4eb5cf8f6d7958b0e7c9f491cb97
dvillazu27 <61160481+dvillazu27@users.noreply.github.com>
2020-02-26 20:34:47 -0600

    Punto 7 Tarea
    Codigo con errores detectables por flake8

---
+++ 3Errores.py
@@ -0,0 +1,24 @@
+def Menu():
+    a=0
+    b=2
+    c=9
+
+    R=c+b
+
+    print("      *****MENU*****")
+    print("      *            *")
+    print("      *            *")
+    print("      * BIENVENIDO *")
+    print("      *            *")
+    print("      *            *")
+    print("      *            *")
+    print("      *            *")
+    print("      *   DIGITE   *")
+    print("      *     SU     *")
+     
+    print("      *    ORDEN   *")
+    print("      *            *")
+    print("      *            *")
+    print("      *            *")
+    print("      *            *")
+    print("      ****GRACIAS***")
